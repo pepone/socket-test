f28665205e7c08c36324b0f8144ea91f4eade8f5
Jose <jose@zeroc.com>
2021-05-31 10:43:51 +0200

    Socket test

---
+++ client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var endpoint = new DnsEndPoint("localhost", 10000);
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            await socket.ConnectAsync(endpoint);
+            Console.WriteLine($"connected to: {socket.LocalEndPoint}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var endpoint = new IPEndPoint(IPAddress.IPv6Loopback, 10000);
+            var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.DualMode = true;
+            socket.ExclusiveAddressUse = true;
+
+            socket.Bind(endpoint);
+            socket.Listen();
+            Console.WriteLine($"listening at: {socket.LocalEndPoint}");
+            Console.ReadLine();
+        }
+    }
+}
