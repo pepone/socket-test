ea33eff05cb5c338427852893561938b445d8357
Jose <jose@zeroc.com>
2021-05-31 16:00:45 +0200

    Simplify test case

--- test/Program.cs
+++ test/Program.cs
@@ -8,16 +8,29 @@ namespace client
 {
     class Program
     {
+        public static void CreateServer()
+        {
+            var endpoint = new IPEndPoint(IPAddress.Any, 62146);
+            var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.ExclusiveAddressUse = true;
+
+            socket.Bind(endpoint);
+            socket.Listen();
+            Console.WriteLine($"listening at: {socket.LocalEndPoint}");
+        }
+
         static async Task Main(string[] args)
         {
             try
             {
+                CreateServer();
                 var endpoint = new DnsEndPoint("localhost", 62146);
                 var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
 
                 Console.WriteLine($"Connecting to: {endpoint}");
                 await socket.ConnectAsync(endpoint, CancellationToken.None);
                 Console.WriteLine($"socket.AddressFamily: {socket.AddressFamily}");
+                // Accessing LocalEndpoint here causes the crash
                 Console.WriteLine($"connected to: {socket.LocalEndPoint}");
                 Console.ReadLine();
                 socket.Close();
